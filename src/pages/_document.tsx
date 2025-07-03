3f398f10816e96dbe284e98457c8bb52c47c7bb2
Azfar Qadri <imazfarqadri@gmail.com>
2025-07-03 23:51:09 +0500

    Fixed _document.tsx syntax errors and proper Head structure

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,34 +3,35 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      `<Head />
-      <meta data-rh="true" name="theme-color" content="#FFFFFF" />
-      <meta name="google-site-verification" content="SY2-UJ6CktAAJnM7sSiieGZEPCNyXtW2nLwoEVGIBHw" />
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZCNM7FL8F0"></script>`
-<script
-  dangerouslySetInnerHTML={{
-    __html: `
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'G-ZCNM7FL8F0', {
-        page_path: window.location.pathname
-      });
-      console.log('🟢 Google Analytics initialized:', 'G-ZCNM7FL8F0');
-    `,
-  }}
-/>
+      <Head>
+        <meta data-rh="true" name="theme-color" content="#FFFFFF" />
+        <meta name="google-site-verification" content="SY2-UJ6CktAAJnM7sSiieGZEPCNyXtW2nLwoEVGIBHw" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZCNM7FL8F0"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-ZCNM7FL8F0', {
+                page_path: window.location.pathname
+              });
+              console.log('🟢 Google Analytics initialized:', 'G-ZCNM7FL8F0');
+            `,
+          }}
+        />
 
-      {/* <script
-        crossOrigin="anonymous"
-        src="//unpkg.com/react-scan/dist/auto.global.js"
-      /> */}
-      {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" /> */}
-      {/* <link
-        href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap"
-        rel="stylesheet"
-      /> */}
+        {/* <script
+          crossOrigin="anonymous"
+          src="//unpkg.com/react-scan/dist/auto.global.js"
+        /> */}
+        {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" /> */}
+        {/* <link
+          href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap"
+          rel="stylesheet"
+        /> */}
+      </Head>
       <body>
         <Main />
         <NextScript />
